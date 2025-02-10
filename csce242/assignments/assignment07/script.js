4f6985077d3f4605d3aa00e3bc1161ad1cc1ab8c
Lauren Murphy <lmmurphy0808@users.noreply.github.com>
2025-02-09 22:25:20 -0500

    Added Assignemnt07

---
+++ csce242/assignments/assignment07/script.js
@@ -0,0 +1,33 @@
+/* Hello Button */
+const helloButton = document.getElementById('helloButton');
+const container = document.querySelector('.container'); // Select the container
+
+helloButton.addEventListener('click', () => {
+  const newHello = document.createElement('div'); // Create a new div element
+  newHello.classList.add('popup'); // Add the 'popup' class for styling
+  newHello.textContent = 'Hello!'; // Set the text of the new "Hello!" message
+  container.appendChild(newHello); // Append the new "Hello!" message to the container
+});
+
+/* Color Selector */
+const colorSelector = document.getElementById('colorSelector');
+const star = document.getElementById('star');
+
+colorSelector.addEventListener('input', (event) => {
+    star.style.backgroundColor = event.target.value;
+});
+
+
+
+// Function to generate random colors
+function getRandomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);  // Generates a random color in hex format
+    return randomColor;
+}
+
+/* Random Image Generator */
+const image = document.getElementById('image');
+
+image.addEventListener('click', () => {
+    image.src = `https://picsum.photos/200/200?random=${Math.random()}`; 
+});
