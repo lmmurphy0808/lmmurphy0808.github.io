446c6130c2e609339f09d098fc9cba777da59bf2
Lauren Murphy <lmmurphy0808@users.noreply.github.com>
2025-03-06 09:18:18 -0500

    Added content to json

--- json/cookies.js
+++ json/cookies.js
@@ -1,1 +1,58 @@
-h
+[
+    {
+        "name": "Double Chocolate Chip Cookies",
+        "image": "",
+        "difficulty": "3.0",
+        "time": "90 mins",
+        "reviews": ["my go-to","classic","never fails"]
+    },
+    {
+        "name": "Peanut Butter Blossom Cookies",
+        "image": "",
+        "difficulty": "4.0",
+        "time": "45 mins",
+        "reviews": ["great for holiday parties","doesn't disappoint","calling all peanut butter lovers"]
+    },
+    {
+        "name": "Red Velvet Crinkle Cookies",
+        "image": "",
+        "difficulty": "4.0",
+        "time": "75 mins",
+        "reviews": ["holiday cookies that everyone loves","best cookies I've ever had","so festive and so fun"]
+    },
+    {
+        "name": "Confetti Cookies",
+        "image": "",
+        "difficulty": "3.0",
+        "time": "45 mins",
+        "reviews": ["perfect for July 4th","yummy cookies","can't wait to make these again"]
+    },
+    {
+        "name": "Lemon Poppyseed Cookies",
+        "image": "",
+        "difficulty": "3.5",
+        "time": "45 mins",
+        "reviews": ["recipe is spot on","perfect if you want a super sweet yet refreshing treat","perfection"]
+    },
+    {
+        "name": "Chocolate Chip and M&M Cookies",
+        "image": "",
+        "difficulty": "3.0",
+        "time": "60 mins",
+        "reviews": ["came out perfect and taste delicious","these were a hit","best M&M cookies I've ever made"]
+    },
+    {
+        "name": "Classic Sugar Cookies",
+        "image": "",
+        "difficulty": "3.0",
+        "time": "45 mins",
+        "reviews": ["great to always have a batch on-hand","quick and easy to whip up","will definitely be making again"]
+    },
+    {
+        "name": "Snickerdoodle Cookies",
+        "image": "",
+        "difficulty": "3.0",
+        "time": "45 mins",
+        "reviews": ["recipe is definitely a keeper","fun, easy, tasty","soft and chewy"]
+    }
+]
---
+++ json/json-cookies.js
@@ -0,0 +1,3 @@
+const getCookies = async() => {
+    const url = "https://lmmurphy0808.github.io/json/cookies.js"
+}
