df1b9c98737696e7f7008c08bbce5c5e78ba4522
Lauren Murphy <lmmurphy0808@users.noreply.github.com>
2025-03-06 08:33:01 -0500

    Added json files

---
+++ csce242/exercises/04_js/js/json-shoes.js
@@ -0,0 +1,22 @@
+const getShoes = async() => {
+    const url = "https://portiaportia.github.io/json/shoes.json";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    }  catch(error){
+        console.log(error);
+    }
+};
+
+const showShoes = () => {
+    const shoes = getShoes();
+    const shoesSection = document.getElementById("shoes-section")
+
+    shoes.forEach((shoe)=>{
+        shoesSection.append(shoe.name);
+
+    });
+};
+
+showShoes();
