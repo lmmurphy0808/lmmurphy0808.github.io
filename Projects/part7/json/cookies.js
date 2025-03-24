0f0529bf8c986fa23e607e37294abc34c32de146
Lauren Murphy <lmmurphy0808@users.noreply.github.com>
2025-03-23 22:12:38 -0400

    Added last part of website updates

---
+++ Projects/part7/json/cookies.js
@@ -0,0 +1,58 @@
+[
+    {
+        "name": "Double Chocolate Chip Cookies",
+        "image": "images/doublechocolatechipcookies.jpg",
+        "difficulty": "3.0",
+        "time": "90 mins",
+        "reviews": ["my go-to","classic","never fails"]
+    },
+    {
+        "name": "Peanut Butter Blossom Cookies",
+        "image": "images/peanutbutterblossomcookies.jpg",
+        "difficulty": "4.0",
+        "time": "45 mins",
+        "reviews": ["great for holiday parties","doesn't disappoint","calling all peanut butter lovers"]
+    },
+    {
+        "name": "Red Velvet Crinkle Cookies",
+        "image": "images/redvelvetcrinklecookies.jpg",
+        "difficulty": "4.0",
+        "time": "75 mins",
+        "reviews": ["holiday cookies that everyone loves","best cookies I've ever had","so festive and so fun"]
+    },
+    {
+        "name": "Confetti Cookies",
+        "image": "images/confetticookies.jpg",
+        "difficulty": "3.0",
+        "time": "45 mins",
+        "reviews": ["perfect for July 4th","yummy cookies","can't wait to make these again"]
+    },
+    {
+        "name": "Lemon Poppyseed Cookies",
+        "image": "images/lemonpoppyseedcookies.jpg",
+        "difficulty": "3.5",
+        "time": "45 mins",
+        "reviews": ["recipe is spot on","perfect if you want a super sweet yet refreshing treat","perfection"]
+    },
+    {
+        "name": "Chocolate Chip and M&M Cookies",
+        "image": "images/chocolatechipm+mcookies.jpg",
+        "difficulty": "3.0",
+        "time": "60 mins",
+        "reviews": ["came out perfect and taste delicious","these were a hit","best M&M cookies I've ever made"]
+    },
+    {
+        "name": "Classic Sugar Cookies",
+        "image": "images/classicsugarcookies.jpg",
+        "difficulty": "3.0",
+        "time": "45 mins",
+        "reviews": ["great to always have a batch on-hand","quick and easy to whip up","will definitely be making again"]
+    },
+    {
+        "name": "Snickerdoodle Cookies",
+        "image": "images/snickerdoodlecookies.jpg",
+        "difficulty": "3.0",
+        "time": "45 mins",
+        "reviews": ["recipe is definitely a keeper","fun, easy, tasty","soft and chewy"]
+    }
+]
---
+++ Projects/part7/json/json-cookies.js
@@ -0,0 +1,34 @@
+const getCookies = async() => {
+    const url = "https://lmmurphy0808.github.io/Projects/part6/json/cookies.js";
+
+    try {
+        const response = await fetch(url);
+        return response.json();
+    } catch(error){
+        console.log(error);
+    }
+};
+
+const showCookies = async() => {
+    const cookies = await getCookies();
+    const cookieRecipes = document.getElementById("recipes");
+
+    cookies.forEach((cookie)=>{
+        const section = document.createElement("section");
+        cookieRecipes.append(section);
+        const h3 = document.createElement("h3");
+        h3.innerHTML = cookie.name;
+        section.append(h3);
+
+        const ul = document.createElement("ul");
+        section.append(ul);
+
+        cookie.reviews.forEach((review)=>{
+            const li = document.createElement("li");
+            li.append(review);
+            ul.append(li);
+        });
+    });
+};
+
+showCookies();
